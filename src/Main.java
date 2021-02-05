0cd022881569ee7bc7869c8df04cc41a192ea92e
Thomas Truelsen <tt91@live.dk>
2021-02-05 11:59:49 +0100

    Title

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui/views/LoginViewController.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Student Registration");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
