1e7252892ec5ee060c237f137d88d552c414656c
m1stertt <52206366+m1stertt@users.noreply.github.com>
2021-02-05 11:49:23 +0100

    Initial.

---
+++ src/gui/controllers/LoginView.java
@@ -0,0 +1,12 @@
+package gui.controllers;
+
+import com.sun.tools.javac.Main;
+
+public class LoginView {
+
+    private Main main;
+
+    public void setMainApp(Main mainApp){
+        this.main = mainApp;
+    }
+}
