2cecedc07499e5fa9364a67ca7d539d0e137b71b
JeppeMadsen97 <71627862+JeppeMadsen97@users.noreply.github.com>
2021-02-07 18:18:54 +0100

    Semi-final loginView.fxml

--- src/gui/controllers/LoginViewController.java
+++ src/gui/controllers/LoginViewController.java
@@ -1,10 +1,29 @@
 package gui.controllers;
 
 import com.sun.tools.javac.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 
 public class LoginViewController {
 
+    @FXML
+    private ImageView EASV;
+
+    @FXML
+    void loginStudent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginTeacher(ActionEvent event) {
+
+    }
+
+
     private Main main;
+
 
     public void setMainApp(Main mainApp){
         this.main = mainApp;
