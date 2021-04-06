2baeb6a245c44535ac737db94c74f970ccbcc380
Natorden <71587437+Natorden@users.noreply.github.com>
2021-04-06 13:36:31 +0200

    small changes focused around the more info,
    cant do much atm, as there is nothing displayed on the students of the class

--- src/gui/controllers/StudentInformationController.java
+++ src/gui/controllers/StudentInformationController.java
@@ -3,29 +3,59 @@ package gui.controllers;
 import be.Student;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
-public class StudentInformationController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    protected boolean editMode = false;
+public class StudentInformationController implements Initializable {
+
     public RadioButton presentSelect;
     public RadioButton absentSelect;
     public ToggleGroup attendance;
     public LineChart absenceDisplay;
     public JFXButton closesWindow;
+    public Label studentNameDisplay;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        drawStudentAttendenceChart();
+    }
 
     public void savesAndCloses(ActionEvent actionEvent) {
+
+        //todo add code to save and close the window
+
+        Stage window = (Stage) closesWindow.getScene().getWindow();
+        window.close();
+
     }
 
     public void setsAttendance(ActionEvent actionEvent) {
     }
 
     public void attendanceEdit(Student student){
-        editMode = true;
 
         //todo add stuff that allows editing of attendance
 
     }
+
+    public void drawStudentAttendenceChart(){
+        Axis<String> xAxis = absenceDisplay.getXAxis();
+        xAxis.setTickLabelRotation(1);
+        xAxis.setLabel("Days of the Week");
+        Axis<String> yAxis = absenceDisplay.getYAxis();
+        yAxis.setLabel("Percentage Absence");
+
+        //todo add information from database on students abscene
+    }
+
+
 }
