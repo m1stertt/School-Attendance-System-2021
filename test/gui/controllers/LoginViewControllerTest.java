b7e701babb1f1fb905ba2e7e198f6b9f993ad6c8
Natorden <71587437+Natorden@users.noreply.github.com>
2021-04-21 12:35:52 +0200

    tested something, but doesnt work

---
+++ test/gui/controllers/LoginViewControllerTest.java
@@ -0,0 +1,34 @@
+package gui.controllers;
+
+import be.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.instrument.IllegalClassFormatException;
+import java.util.IllegalFormatException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginViewControllerTest {
+
+    @DisplayName("Tests if the log in works")
+    @Test
+    void attendanceLogin() {
+
+        User testUser = new User(1, "Test", "User");
+        String login = testUser.getFirstName();
+
+        Exception error = Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            testUser.equals(login);
+        });
+        
+        Assertions.assertEquals(login,testUser.getFirstName());
+
+        String expectedMessage = "Username is invalid";
+        String actualMessage = error.getMessage();
+        Assertions.assertEquals(expectedMessage,actualMessage);
+
+    }
+
+}
