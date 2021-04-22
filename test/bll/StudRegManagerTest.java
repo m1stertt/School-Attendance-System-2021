1a6e8e938436511c80d8b0dddd9bc2696d5d58ee
m1stertt <52206366+m1stertt@users.noreply.github.com>
2021-04-22 14:57:36 +0200

    Some test stuff.

--- test/bll/StudRegManagerTest.java
+++ test/bll/StudRegManagerTest.java
@@ -1,17 +1,22 @@
 package bll;
 
+import be.Course;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class StudRegManagerTest {
 
     @Test
     void getCourseAbsenceDate() {
+
     }
 
     @Test
     void getWeekdayAttendanceData() {
+
     }
 
     @Test
@@ -20,5 +25,15 @@ class StudRegManagerTest {
 
     @Test
     void getAbsenceData() {
+        double expectedSum = 0.5;
+        double sum = 0;
+        StudRegTestDAO studRegTestDAO = new StudRegTestDAO();
+        List<Course> courses = studRegTestDAO.getAllStudentCourses();
+
+        for (Course ignored : courses) {
+            sum += (double) studRegTestDAO.getStudentAttendanceDaysInSemesterCourse() / studRegTestDAO.getCourseDaysInSemesterCourseUntilNow();
+        }
+        double d = sum / courses.size();
+        Assertions.assertEquals(expectedSum, d);
     }
 }
---
+++ test/bll/StudRegTestDAO.java
@@ -0,0 +1,28 @@
+package bll;
+
+import be.Course;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class StudRegTestDAO {
+
+    public List<Course> getAllStudentCourses() {
+        List<Course> allCourses = new ArrayList<>();
+        allCourses.add(new Course(1, "DBOS2.AB.21", Calendar.getInstance().getTime(), Calendar.getInstance().getTime()));
+        allCourses.add(new Course(2, "ITO2.AB.21", Calendar.getInstance().getTime(), Calendar.getInstance().getTime()));
+        allCourses.add(new Course(3, "SCO2.B.21", Calendar.getInstance().getTime(), Calendar.getInstance().getTime()));
+        allCourses.add(new Course(4, "SDE2.B.21", Calendar.getInstance().getTime(), Calendar.getInstance().getTime()));
+
+        return allCourses;
+    }
+
+    public Integer getStudentAttendanceDaysInSemesterCourse() {
+        return 10;
+    }
+
+    public double getCourseDaysInSemesterCourseUntilNow() {
+        return 20;
+    }
+}
